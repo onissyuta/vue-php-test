080e08481faf97934bffb161a8e95e0d8e0dfa2a
onissyuta <dev@pixelog.net>
2023-07-05 17:44:59 +0900

    [clean] リファクタリング

--- server/api/index.php
+++ server/api/index.php
@@ -5,16 +5,15 @@ require_once('MessageDAO.php');
 header('content-type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *"); // クロスオリジン許可
 
+$db = new MessageDAO();
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	$db = new MessageDAO();
     echo json_encode($db->findAll(), JSON_UNESCAPED_UNICODE);
 } elseif($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if(isset($_POST['name']) && isset($_POST['message'])) {
-        $db = new MessageDAO();
         $db->create(new Message($_POST['name'], $_POST['message']));
     }
 } elseif($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-    $db = new MessageDAO();
     $db->deleteAll();
 }
 
