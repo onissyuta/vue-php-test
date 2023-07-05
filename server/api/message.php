f39ae39286b6f10ddf7dcb73693ce779eb8b543c
onissyuta <dev@pixelog.net>
2023-07-05 16:03:06 +0900

    [update] DAO追加

--- server/api/message.php
+++ server/api/message.php
@@ -1,36 +1,7 @@
 <?php
-
-$array = [
-    'tokyo' => [
-        '品川',
-        '五反田',
-        '三軒茶屋',
-        '四谷'
-    ],
-    'kanagawa' => [
-        '横浜',
-        '相模原',
-        '湘南',
-        '鎌倉'
-    ],
-    'saitama' => [
-        '所沢',
-        '狭山',
-        '川口',
-        '浦和',
-        '小手指',
-        '飯能'
-    ]
-];
-
-
 header('content-type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *"); // クロスオリジン許可
 
-echo json_encode($array, JSON_UNESCAPED_UNICODE);
-
-
-$db = new SQLite3('sqlite.db');
 /*
 $db->exec('CREATE TABLE message (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -47,8 +18,7 @@ var_dump($result->fetchArray());
 // 
 
 
-
-class message {
+class Message {
     private $id = "";
     private $name = "";
     private $message = "";
@@ -79,13 +49,58 @@ class message {
 }
 
 
+class MessageDAO {
+    private $db = null;
+
+    // コンストラクタ
+    function __construct(){
+        $this->db = new SQLite3('sqlite.db');
+    }
+
+    // デストラクタ
+    function __destruct(){
+        $this->db->close(); // 接続を閉じる
+    }
 
 
+    public function create($obj){
+        $pstmt = $this->db->prepare('INSERT INTO message (name, message) VALUES (:name, :message)');
+        $pstmt->bindValue(':name', $obj->getName());
+        $pstmt->bindValue(':message', $obj->getMessage());
+        $pstmt->execute();
+    }
+
+
+    public function findAll(){
+        $result = $this->db->query('SELECT * FROM message');
+        return $result->fetchArray();
+
+    }
+
+    public function findOneById($id){
+        $pstmt = $this->db->prepare('SELECT * FROM message WHERE id = :id');
+        $pstmt->bindValue(':name', $id);
+        return $pstmt->execute();
+    }
+
+
+    public function deleteAll(){
+        return $this->db->exec('DELETE * FROM message');
+    }
+
+}
+
+
+/*
 
 $obj = new message();
 $obj->setName("あああ");
 var_dump($obj);
+*/
 
+$db = new MessageDAO();
+var_dump($db->findAll());
+var_dump($db->findOneById(1));
 
 
 ?>
