aca81c6695a5703f07db31eff02a5c966b7e6c5b
onissyuta <dev@pixelog.net>
2023-07-06 10:51:49 +0900

    [update] クライアントアプリ作成

--- client/vue-app/src/App.vue
+++ client/vue-app/src/App.vue
@@ -1,16 +1,61 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const name = ref("")
+const message = ref("")
+const serverData = ref("")
+
+fetchAllMessages();
+
+function fetchAllMessages(){
+    fetch("http://localhost:800/api/index.php")
+    .then(
+        r => r.json().then(json => {
+          serverData.value = json;
+        })
+    )
+    .catch(r => console.log("通信失敗" , r));
+}
+
+function submit() {
+    fetch("http://localhost:800/api/index.php",
+        {
+            "method": "POST",
+            "body": `name=${name.value}&message=${message.value}`,
+            "headers": {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            }
+        }
+    )
+    .then(r => {
+        fetchAllMessages();
+        message.value = "";
+    })
+    .catch(r => console.log("通信失敗" , r));
+}
+
+function clearAll(){
+    fetch("http://localhost:800/api/index.php",
+        {
+            "method": "DELETE"
+        }
+    )
+    .then(r => fetchAllMessages())
+    .catch(r => console.log("通信失敗" , r));
+}
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <form @submit.prevent="submit">
+        <input type="text" placeholder="名前" size="20" v-model="name">
+        <input type="text" placeholder="本文" size="60" v-model="message">
+        <button type="submit">送信</button>
+    </form>
+    <button type="button" @click="clearAll">Clear</button>
+    <hr>
+    <div v-for="e in serverData">
+        {{e.name}}: {{e.message}}
     </div>
-  </header>
-
-  <main>
-  </main>
 </template>
 
 <style scoped>
